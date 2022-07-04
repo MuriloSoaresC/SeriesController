0153c2f09021e4ee1a4ddd8a60c75742f1a799dd
Murilo Soares <murilosoaresc3@gmail.com>
2022-07-04 13:49:12 -0300

    Inclusão formulário próprio de edição e exclusão de component padrão

--- controle-series/resources/views/series/edit.blade.php
+++ controle-series/resources/views/series/edit.blade.php
@@ -1,3 +1,13 @@
 <x-layout title="Editar Série '{!! $serie->nome !!}'">
-    <x-series.form :action="route('series.update', $serie->id)" :nome="$serie->nome" :update="true" />
+
+    <form action="{{ route('series.update', $serie->id) }}" method="post">
+        @csrf
+        <div class="mb-3">
+        <label for="nome" class="form-label">Nome:</label>
+        <input type="text" id="nome" name="nome" class="form-control" value="{{ old('nome') }}">
+        </div>
+        @method('PUT')
+        <button type="submit" class="btn btn-primary">Editar</button>
+    </form>
+
 </x-layout>
